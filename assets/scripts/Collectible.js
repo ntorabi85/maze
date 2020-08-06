9945f6acba530e108225d578e53148a1d1d78ec7
Navid <ntorabi85@gmail.com>
2020-08-06 18:30:45 +0430

    Gamepad changed, Enemy added, Collectible added

---
+++ assets/scripts/Collectible.js
@@ -0,0 +1,20 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.node.color = new cc.Color(0, 255, 0, 255);
+    },
+
+    // start () {
+
+    // },
+
+    // update (dt) {},
+});
---
+++ assets/scripts/Enemy.js
@@ -0,0 +1,20 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.node.color = new cc.Color(255, 0, 0, 255);
+    },
+
+    // start () {
+
+    // },
+
+    // update (dt) {},
+});
